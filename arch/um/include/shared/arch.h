3cad1ccd7adc9634bc98cf57df0972cafab798ba
Physics Enthusiast <118960463+physics-enthusiast@users.noreply.github.com>
2025-02-03 20:11:42 +0800

    Update arch.h

--- arch/um/include/shared/arch.h
+++ arch/um/include/shared/arch.h
@@ -8,6 +8,7 @@
 
 #include <sysdep/ptrace.h>
 
+extern void arch_load_cpu_info(void);
 extern void arch_check_bugs(void);
 extern int arch_fixup(unsigned long address, struct uml_pt_regs *regs);
 extern void arch_examine_signal(int sig, struct uml_pt_regs *regs);
