86d35373cea1f4aad8939b53f5217db2ab06b1e6
Physics Enthusiast <118960463+physics-enthusiast@users.noreply.github.com>
2025-02-04 18:26:04 +0800

    Create page.h

---
+++ arch/x86/um/asm/page.h
@@ -0,0 +1,19 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (C) 2000 - 2003 Jeff Dike (jdike@addtoit.com)
+ * Copyright 2003 PathScale, Inc.
+ */
+
+#ifndef __UM_PAGE_H
+#define __UM_PAGE_H
+
+#include <asm/page-generic.h>
+
+#ifndef __ASSEMBLY__
+
+#ifdef CONFIG_X86_32
+#define __HAVE_ARCH_GATE_AREA 1
+#endif
+
+#endif	/* __ASSEMBLY__ */
+#endif	/* __UM_PAGE_H */
