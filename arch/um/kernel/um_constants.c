722406fcbd9473d8f720fe93f856236e912c63e9
Physics Enthusiast <118960463+physics-enthusiast@users.noreply.github.com>
2025-03-07 17:29:31 +0800

    Delete arch/um/kernel/um_constants.c

--- arch/um/kernel/um_constants.c
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: GPL-2.0
-#include <linux/types.h>
-#include <linux/kbuild.h>
-
-void um_constants(void)
-{
-	DEFINE(UM_UNSIGNED_INT_BITS, sizeof(unsigned int));
-  DEFINE(UM_UINTPTR_T_BITS, sizeof(uintptr_t));
-}
