645e31d9dbeb38366b75eaa32e077990cbc2b68b
Physics Enthusiast <118960463+physics-enthusiast@users.noreply.github.com>
2025-03-10 22:15:28 +0800

    Update processor.c

--- arch/x86/um/processor.c
+++ arch/x86/um/processor.c
@@ -4,20 +4,20 @@
 
 #include <asm/cpufeature.h>
 
-struct cpuinfo_x86 boot_cpu_data_extended = {
+struct cpuinfo_x86 boot_cpu_data = {
 	.x86_capability		= { 0 }
 };
 
-EXPORT_SYMBOL(boot_cpu_data_extended);
+EXPORT_SYMBOL(boot_cpu_data);
 
-void arch_show_cpuinfo(struct seq_file *m)
+void um_subarch_cpuinfo(struct seq_file *m)
 {
 	int i;
 
-	seq_printf(m, "fpu\t\t: %s\n", cpu_has(&boot_cpu_data_extended, X86_FEATURE_FPU) ? "yes" : "no");
+	seq_printf(m, "fpu\t\t: %s\n", cpu_has(&boot_cpu_data, X86_FEATURE_FPU) ? "yes" : "no");
 	seq_printf(m, "flags\t\t:");
 	for (i = 0; i < 32*NCAPINTS; i++)
-		if (cpu_has(&boot_cpu_data_extended, i) && (x86_cap_flags[i] != NULL))
+		if (cpu_has(&boot_cpu_data, i) && (x86_cap_flags[i] != NULL))
 			seq_printf(m, " %s", x86_cap_flags[i]);
 	seq_printf(m, "\n");
 }
