f737fc02cf4b6bdac9a658263a6d73f23657be4e
Physics Enthusiast <118960463+physics-enthusiast@users.noreply.github.com>
2025-03-10 08:17:08 +0800

    Update um_host.h

--- arch/um/include/shared/um_host.h
+++ arch/um/include/shared/um_host.h
@@ -5,12 +5,17 @@
 
 #include <um_host_arch.h>
 
+#define ARRAY_SIZE(x) (sizeof(x) / sizeof((x)[0]))
+
 struct boot_params {
 	char machine[65];
 	int nr_cpus;
 	int cache_alignment;
 	int page_shifts[8];
-	arch_boot_params arch_data;
+	char extensions[ARRAY_SIZE(UM_ARCH_EXTENSIONS)];
+	char constraints[ARRAY_SIZE(UM_ARCH_CONSTRAINTS)];
+	char syscalls[ARRAY_SIZE(UM_ARCH_SYSCALLS)];
+	um_arch_boot_params arch_data;
 };
 
 extern struct boot_params um_host_params;
